64747bab3fbfcf4b7cdb592bd87462da4635ccca
Girish Koliki <girish@fusecup.co>
2026-04-30 23:30:26 +0100

    cleanup

--- ops/prod/variable.tf
+++
@@ -1,6 +0,0 @@
-# All of this variables are set in Terraform Cloud
-variable "digitalocean_token" {}
-variable "g_ssh_key" {}
-variable "github_ssh_key" {}
-variable "cloudflare_api_token" {}
-variable "cloudflare_zone_id" {}
